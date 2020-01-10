42e9318d328ff77b19407b78852a991285f65cd1
Andrey Alekseev <ilekseev@gmail.com>
2020-01-11 02:15:11 +0300

    fix: make tables name right feat: add all necessary entities

--- LMS/db_tables_entity.py
+++ LMS/db_tables_entity.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -18,6 +18,56 @@ class User(Base):
     def __repr__(self):
         return f"<User(name={self.name}, family_name={self.family_name}, status={self.status}," \
                f"email={self.email}, phone={self.phone})>"
+
+
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+
+    def __repr__(self):
+        return "<User(name='%s', description='%s')>" % (
+            self.name, self.description)
+
+
+class Group(Base):
+    __tablename__ = 'groups'
+
+    num = Column(Integer, primary_key=True)
+    degree = Column(String)
+    grade = Column(Integer)
+
+    def __repr__(self):
+        return "<User(num='%d', degree='%s', grade='%d')>" % (
+            self.num, self.degree, self.grade)
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    group_num = Column(Integer, ForeignKey('groups.num'))
+    pay_flg = Column(Boolean)
+
+    def __repr__(self):
+        return "<User(name='%s', group_number='%d')>" % (
+            User.name, self.group_num)
+
+
+class Material(Base):
+    __tablename__ = 'materials'
+
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    name = Column(String)
+    text = Column(String)
+    date = Column(DateTime)
+    course_id = Column(Integer, ForeignKey('courses.id'))
+
+    def __repr__(self):
+        return "<User(name='%s', name='%s', 'text'='%s')>" % (
+            self.name, self.name, self.text)
 
 
 class Homework(Base):
