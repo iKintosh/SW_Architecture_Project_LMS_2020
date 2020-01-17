35d004126c1293ffafc93885b50509e1291707d0
Restyled.io <commits@restyled.io>
2020-01-17 20:24:23 +0000

    Restyled by black

--- LMS/controller/routes.py
+++ LMS/controller/routes.py
@@ -1,9 +1,9 @@
 from LMS import api
 from LMS.controller import signup, login, user
 
-api.add_resource(signup.SignupApi, '/signup')
-api.add_resource(login.LoginApi, '/login')
-api.add_resource(user.UserListAPI, '/user')
-api.add_resource(user.UserAPI, '/user/<int:url_id>')
-'''api.add_resourse(user.UserCoursesApi, '/user/courses')
-'''
+api.add_resource(signup.SignupApi, "/signup")
+api.add_resource(login.LoginApi, "/login")
+api.add_resource(user.UserListAPI, "/user")
+api.add_resource(user.UserAPI, "/user/<int:url_id>")
+"""api.add_resourse(user.UserCoursesApi, '/user/courses')
+"""
